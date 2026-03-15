24641396ac179135a2aaaa07663981a089315c70
tosiaa <tosiaszutko@gmail.com>
2026-03-15 13:01:52 +0100

    Implement SASA and Rg calculations in descriptors.py
    Added functions to calculate Solvent Accessible Surface Area (SASA) and Radius of Gyration (Rg) for protein structures.

--- src/engine/descriptors.py
+++ src/engine/descriptors.py
@@ -1,4 +1,5 @@
 import biotite.structure as struct 
+import numpy as np
 
 def calc_ligand_buried_surface(
         pocket_array: struct.AtomArray,
@@ -10,6 +11,32 @@ def calc_ligand_buried_surface(
     ligand_in_pocket_sasa = pocket_sasa[ligand_in_pocket_mask].sum()
 
     return ligand_sasa - ligand_in_pocket_sasa
-
+        
+"""
+Solvent Accessible Surface Area (SASA):
+Measures how much of the protein's surface area is physically 
+exposed to the surrounding solvent (usually water).
+"""
+def calc_sasa_protein(structure: struct.AtomArray) -> float:
+        protein_mask = struc.filter_amino_acids(structure) #removing ligands, water molecules, etc.
+        protein_array = structure[protein_mask]
+        
+        sasa_protein = struc.sasa(protein_array)
+        total_sasa = np.nansum(sasa_protein) 
+        
+        return total_sasa
+        
+"""
+Radius of Gyration (Rg):
+Measures the overall size and compactness of the protein structure.
+It calculates the mass-weighted average distance of all atoms from 
+the protein's center of mass.
+- Low Rg: The protein is tightly folded and compact
+- High Rg: The protein is extended, unfolded, or highly flexible
+"""
+def calc_gyration_radius(protein_array: struct.AtomArray) -> float:
+        gyration_radius = struct.gyration_radius(protein_array, masses=none)
+        
+        return gyration_radius
 
 
