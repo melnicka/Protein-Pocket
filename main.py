aadf1aacbeec227a41995c0242c0d837f4ece4ec
artuzik21-del <artuzik21@gmail.com>
2026-05-19 23:04:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ if __name__ == '__main__':
     # entry.save_pocket_cif_files()
     metadata = entry.extract_metadata()
     # TEST PUBMED
-    #protein_name = extract_protein_name(cif_path)
+    #protein_name = extract_protein_name(cif_path, "4HHB")
     papers = fetch_protein_papers("Human Deoxyhaemoglobin")
     print(" PAPERS ")
     for link, abstract in papers:
         print(link)
         print(abstract)
         print("-" * 80)
+
 
 
